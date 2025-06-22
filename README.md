691e065a9ff7b0ed2ca3d6c3957be9433de3676f
wlrma0108 <dlwlrma0108@gmail.com>
2025-06-22 23:09:45 +0900

    django

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # sinkhole_mysql
+
+데이터베이스 프로젝트
+
+
+
--- myproject/myservice/views.py
+++ myproject/myservice/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
+from .models import Incident
+from .forms import IncidentForm
 
-# Create your views here.
+def incident_list(request):
+    qs = Incident.objects.all().order_by('-date_reported')
+    return render(request, 'sinkhole/incident_list.html', {'incidents': qs})
+
+def incident_detail(request, pk):
+    inc = get_object_or_404(Incident, pk=pk)
+    return render(request, 'sinkhole/incident_detail.html', {'incident': inc})
+
+def incident_create(request):
+    if request.method == 'POST':
+        form = IncidentForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('incident_list')
+    else:
+        form = IncidentForm()
+    return render(request, 'sinkhole/incident_form.html', {'form': form})
