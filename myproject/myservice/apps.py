84dfe0575661b8e4d3f99b997fa2021d17fa8667
wlrma0108 <dlwlrma0108@gmail.com>
2025-06-24 23:28:51 +0900

    new start

--- myproject/myservice/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class MyserviceConfig(AppConfig):
-    default_auto_field = "django.db.models.BigAutoField"
-    name = "myservice"
--- myproject/myservice/forms.py
+++
@@ -1,7 +0,0 @@
-from django import forms
-from ..myproject.models import Incident
-
-class IncidentForm(forms.ModelForm):
-    class Meta:
-        model = Incident
-        fields = ['district', 'date_reported', 'date_resolved', 'width', 'depth']
